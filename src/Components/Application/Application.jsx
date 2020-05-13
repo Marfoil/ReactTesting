b51b606b1131a87d8716f3cbedbc0aeb93663f31
Marfoil <xavimarfil@gmail.com>
2020-05-13 17:30:27 +0200

    feat: set css-grid and set background

--- src/Components/Application/Application.jsx
+++ src/Components/Application/Application.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { useStyles } from './Application.styles';
-import background from '../../images/background.png';
+import background from '../../../public/img/background.png';
 
 export const Application = () => {
-	const classes = useStyles({ image: background });
-	return (
-		<div className={classes.main}>
-			<div>Title</div>
-			<div>Body</div>
-		</div>
-	);
+    const classes = useStyles({ image: background });
+    return (
+        <div className={classes.main}>
+            <div className={classes.navigation}>Navigation</div>
+            <div>Others</div>
+        </div>
+    );
 };
--- src/Components/Application/Application.styles.js
+++ src/Components/Application/Application.styles.js
@@ -1,9 +1,26 @@
 import { createUseStyles } from 'react-jss';
 
 export const useStyles = createUseStyles({
-	main: {
-		backgroundImage: (props) => `url(${props.image})`,
-		height: '100vh',
-		width: '100vw',
-	},
+    main: {
+        backgroundImage: props => `url(${props.image})`,
+        height: '100vh',
+        width: '100vw',
+        zIndex: -1,
+        color: 'white',
+        backgroundPosition: 'center',
+        display: 'grid',
+        gridTemplateRows: '5% 1fr',
+        gridTemplateAreas: `
+        'navigation'
+        'body'
+        'footer'
+        `,
+    },
+    navigation: {
+        paddingLeft: '1rem',
+        display: 'flex',
+        alignItems: 'center',
+        gridArea: 'navigation',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+    },
 });
