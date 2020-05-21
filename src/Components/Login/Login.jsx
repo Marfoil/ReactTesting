28b4fd488df1047bb92466660edb0de6094d4243
Marfil <xavimarfil@gmail.com>
2020-05-21 10:19:37 +0200

    fix: remove warnings

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import classes from './Login.styles';
 import login from '../../../public/img/login.png';
 import 'normalize.css';
+import { useHistory } from 'react-router-dom';
 
 export const Login = () => {
+    const history = useHistory();
     return (
         <div className={classes.main}>
             <div className={classes.border}>
@@ -11,9 +13,21 @@ export const Login = () => {
                     <img className={classes.logo} src={login} />
                 </p>
                 <form>
-                    <input className={classes.input} placeholder="Username" />
-                    <input className={classes.input} placeholder="Password" type="password" />
-                    <button className={classes.button}>Login</button>
+                    <input autoComplete="username" className={classes.input} placeholder="Username" />
+                    <input
+                        autoComplete="current-password"
+                        className={classes.input}
+                        placeholder="Password"
+                        type="password"
+                    />
+                    <button
+                        onClick={() => {
+                            history.push('/home');
+                        }}
+                        className={classes.button}
+                    >
+                        Login
+                    </button>
                 </form>
             </div>
         </div>
